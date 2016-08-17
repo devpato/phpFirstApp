6aa6a5964e445d4f75fb221fe6638af81967fd8f
Patricio E. Vargas <pevargas_it@valdosta.edu>
2016-08-17 15:04:36 -0400

    first commit for php app

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div class="container">
+			<?php
+				print "<h2>PHP is Fun!</h2>";
+				print "Hello world!!<br>";
+				print "I'm about to learn PHP!";
+			?>
+		</div>
+	</body>
+
+</html>
